647b46f6ec0b29c960c88aa712d2f0fc8c14007d
Francis KOUAHO <kouahofrancis@gmail.com>
2024-08-12 12:20:21 +0200

    feat(back): compose, setup

--- setup.sh
+++ setup.sh
@@ -1,4 +1,8 @@
+#!/bin/bash
+
+docker compose down -v
 curl -fsSL https://get.docker.com -o get-docker.sh
 sudo sh get-docker.sh
 sudo apt-get update
 sudo apt-get install docker-compose-plugin
+sudo docker compose up -d
